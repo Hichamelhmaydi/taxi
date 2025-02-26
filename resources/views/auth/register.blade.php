8a173136d006d87cf0f9e6515854c9156e2dc386
Hichamelhmaydi <ct1594065@gmail.com>
2025-02-26 16:31:55 +0000

    mettre a jour dans le formule de l'inscription

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,27 +11,27 @@
         
             <div>
                 <x-label for="name" value="{{ __('Name') }}" />
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                <x-input id="name" class="block w-full mt-1" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
         
             <div class="mt-4">
                 <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                <x-input id="email" class="block w-full mt-1" type="email" name="email" :value="old('email')" required autocomplete="username" />
             </div>
         
             <div class="mt-4">
                 <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                <x-input id="password" class="block w-full mt-1" type="password" name="password" required autocomplete="new-password" />
             </div>
         
             <div class="mt-4">
                 <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                <x-input id="password_confirmation" class="block w-full mt-1" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
         
             <div class="mt-4">
                 <x-label for="profile_photo" value="{{ __('Profile Photo') }}" />
-                <x-input id="profile_photo" class="block mt-1 w-full" type="file" name="profile_photo" accept="image/*" />
+                <x-input id="profile_photo" class="block w-full mt-1" type="file" name="profile_photo" accept="image/*" />
             </div>
         
             <div class="flex items-center justify-end mt-4">
